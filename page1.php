2897764bd34bc127077ab4b0b169fc3d0318ab94
julien <juliendeman.nc@gmail.com>
2015-09-23 00:42:52 +0200

    4 pages statiques + homepage

---
+++ page1.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <?php echo '<p> Page 1</p>'; ?>
+ </body>
+</html>
---
+++ page4.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Page 4</p>'; ?>
+ </body>
+</html>
