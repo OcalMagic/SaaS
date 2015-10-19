2b54ce56f221b8c61c6655dd5da5db1f092d8299
Floffly <f.royledoux@gmail.com>
2015-10-19 21:22:32 +0200

    Bootstrap en local fonctionnel avec homepage, pagecontact et faq

--- Symfony/src/AE/platformBundle/Controller/AdvertController.php
+++ Symfony/src/AE/platformBundle/Controller/AdvertController.php
@@ -10,12 +10,17 @@ class AdvertController extends Controller
 {
     public function homepageAction()
     {
-        return $this->render('AEplatformBundle:Advert:homepage.html.twig');
+        return $this->render('AEplatformBundle:Advert:homepage.php.twig');
     }
 
     public function contactAction()
     {
-        return $this->render('AEplatformBundle:Advert:pagecontact');
+        return $this->render('AEplatformBundle:Advert:pagecontact.php.twig');
+    }
+
+    public function faqAction()
+    {
+        return $this->render('AEplatformBundle:Advert:faq.php.twig');
     }
 }
 
