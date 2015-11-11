7d889d06a44ab3cb8c9bf2e464c65f97ba3f3bdf
OcalMagic <oliviercoffin@yahoo.fr>
2015-11-11 20:54:55 +0100

    Code mis en place pour le frontend
    ancien code CSS réadapté en Twig pour Symphony

--- Symfony/src/AE/platformBundle/Form/Type/ContactType.php
+++ Symfony/src/AE/platformBundle/Form/Type/ContactType.php
@@ -10,7 +10,8 @@ class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', 'text')
+        $builder->add('civil', 'choice', array('choices' => array('m' => 'Mister', 'f' => 'Miss',), 'required' => true, 'multiple' => false, 'expanded' => true,))
+            ->add('name', 'text')
             ->add('society', 'text')
             ->add('email', 'email')
             ->add('phone', 'text')
